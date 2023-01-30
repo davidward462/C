e9b4049cdf7cc3e8b38580ef9f5bd056774d3a95
David Ward <davidmward@mail.com>
2023-01-29 23:49:36 -0800

    updated structType.c

--- structs/structType.c
+++ structs/structType.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #define LABEL_LEN 32
+
 typedef struct
 {
     int x;
@@ -7,7 +9,25 @@ typedef struct
     char label[LABEL_LEN+1];
 } point;
 
+point CreatePoint(int x, int y, char label[LABEL_LEN+1])
+{
+    point p;
+    p.x = x;
+    p.y = y;
+    strcpy(p.label, label);
+
+    return p;
+}
+
+void PrintPoint (point p)
+{
+    printf("(%d, %d) %s\n", p.x, p.y, p.label);
+}
+
 int main()
-{   point p0 = {0, 0, "zeta"};
+{   
+    point p = CreatePoint(0, 0, "omega");
+    PrintPoint(p);
+
     return 0;
 }
