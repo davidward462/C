8d7ade3a43a714ee589e625f0311f01f2f46bf0f
David Ward <davidmward@mail.com>
2024-03-23 00:53:17 -0700

    test sort on in-order array

--- data-structures/sort/array.c
+++ data-structures/sort/array.c
@@ -32,12 +32,15 @@ void BubbleSort(int array[], int length)
     // Only bother sorting lists with more than one element 
     if (length > 1)
     {
+        int doneSwap = 0;
         int a = array[0];
         int b = array[1];
 
         // Swap values if first is greater than the second
         if(a > b)
         {
+            doneSwap = 1;
+
             // swap
             int t = a;
             a = b;
@@ -54,12 +57,16 @@ int main()
 {
     printf("Begin.\n");
 
-//    int b[] = {1, 2, 3, 4, 5};
-    int tuple[] = {5, 2};
+    int a[] = {5, 2};
+    int b[] = {56, 1203};
 
-    PrintArray(tuple, 2);
-    BubbleSort(tuple, 2);
-    PrintArray(tuple, 2);
+    PrintArray(a, 2);
+    BubbleSort(a, 2);
+    PrintArray(a, 2);
+
+    PrintArray(b, 2);
+    BubbleSort(b, 2);
+    PrintArray(b, 2);
 
     return 0;
 }
